45419ffdb6cca44b029955488e1fd6a0d6232488
Nora Beda <nora.github@bedafamily.com>
2025-10-27 15:53:57 -0700

    check in

--- src/imgui.c
+++ src/imgui.c
@@ -2,6 +2,7 @@
 
 #include "mem.h"
 #include "rasterizer.h"
+#include "mat.h"
 
 #include <string.h>
 
@@ -13,13 +14,39 @@ struct imgui {
     struct pipeline pipeline;
 };
 
+struct imgui_fragment_data {
+    float uv[2];
+    uint32_t color;
+};
+
+struct imgui_vertex {
+    float position[2];
+    struct imgui_fragment_data frag_data;
+};
+
+struct imgui_render_data {
+    float projection[4 * 4];
+};
+
 static void imgui_vertex_shader(const void* const* bindings, const struct shader_context* context,
                                 float* position) {
-    //
+    const struct imgui_vertex* vertex = bindings[0];
+    const struct imgui_render_data* render_data = context->uniform_data;
+
+    float input_pos[4];
+    memcpy(input_pos, vertex->position, 2 * sizeof(float));
+    input_pos[2] = 0.f;
+    input_pos[3] = 1.f;
+
+    mat_dot(render_data->projection, input_pos, 4, 4, 1, position);
+
+    memcpy(context->working_data, &vertex->frag_data, sizeof(struct imgui_fragment_data));
 }
 
 static uint32_t imgui_fragment_shader(const struct shader_context* context) {
     //
+
+    return 0;
 }
 
 static void* imgui_mem_alloc(size_t size, void* user_data) { return mem_alloc(size); }
